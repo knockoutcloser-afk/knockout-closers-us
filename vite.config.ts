504a7de16c8c767b53f32f3df10a90ff088ca005
knockoutcloser-afk <knockoutcloser@gmail.com>
2026-09-04 14:49:33 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,12 @@ export default defineConfig({
   server: {
     preset: "vercel"
   },
+  router: {
+    client: {
+      input: "app/client.tsx"
+    }
+  },
   tanstackStart: {
-    server: { 
-      entry: "app/ssr.tsx" 
-    },
+    server: { entry: "app/ssr.tsx" },
   },
 });
