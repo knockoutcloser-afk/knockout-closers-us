010a822dfca6a7b2a76779cc54b676c50c60afb2
knockoutcloser-afk <knockoutcloser@gmail.com>
2026-09-05 08:18:57 -0600

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
-import "./error-capture";
-import { consumeLastCapturedError } from "./error-capture";
-import { renderErrorPage } from "./error-page";
+import "./src/lib/error-capture";
+import { consumeLastCapturedError } from "./src/lib/error-capture";
+import { renderErrorPage } from "./src/lib/error-page";
 
   fetch: (request: Request, env: unknown, ctx: unknown) => Promise<Response> | Response;
 };
