eb1b543d3b218d664fb072262462e21d94de2daa
knockoutcloser-afk <knockoutcloser@gmail.com>
2026-09-08 20:51:00 -0600

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -132,6 +132,29 @@ function RootShell({ children }: { children: ReactNode }) {
   }}
 />
 
+<script
+            dangerouslySetInnerHTML={{
+              __html: `
+                _linkedin_partner_id = "9671850";
+                window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+                window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+                (function(l) {
+                  if (!l) {
+                    window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+                    window.lintrk.q=[];
+                  }
+                  var s = document.getElementsByTagName("script")[0];
+                  var b = document.createElement("script");
+                  b.type = "text/javascript"; b.async = true;
+                  b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+                  s.parentNode.insertBefore(b, s);
+                })(window.lintrk);
+              `,
+            }}
+          />
+          <noscript>
+            <img height="1" width="1" style={{display: 'none'}} alt="" src="https://px.ads.linkedin.com/collect/?pid=9671850&fmt=gif" />
+          </noscript>        
       </head>
       <body>
         {children}
